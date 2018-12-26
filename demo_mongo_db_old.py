1790e6fe9bd4dd8f7f07fcae0cca621bad2ead2a
Vladyslav Hnatchenko <vladyslav.hnatchenko@eleks.com>
2018-12-26 16:31:40 +0200

    12/26/18

--- demo_mongo_db_old.py
+++ demo_mongo_db_old.py
@@ -1,5 +1,6 @@
 import pymongo
 
+# -> Return the id Field <- #
 
 # Create a database called "mydatabase":
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -28,17 +29,17 @@ x = mycol.insert_many(mylist)
 # print list of the _id values of the inserted documents:
 print(x.inserted_ids)
 
-# # Return a list of your system's databases:
-# print(myclient.list_database_names())
-#
-# # Check if the "customers" collection exists:
-# collist = mydb.list_collection_names()
-# if "customers" in collist:
-#     print("The collection exists.")
-#
-# # Check if "mydatabase" exists:
-# dblist = myclient.list_database_names()
-# if "mydatabase" in dblist:
-#     print("The database exists.")
-# else:
-#     print("We can't create db! \_^:^_/")
+# Return a list of your system's databases:
+print(myclient.list_database_names())
+
+# Check if the "customers" collection exists:
+collist = mydb.list_collection_names()
+if "customers" in collist:
+    print("The collection exists.")
+
+# Check if "mydatabase" exists:
+dblist = myclient.list_database_names()
+if "mydatabase" in dblist:
+    print("The database exists.")
+else:
+    print("We can't create db! \_^:^_/")
