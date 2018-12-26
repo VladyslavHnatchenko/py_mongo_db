579b72d990d2eb06566559b0207b3b5287993ae5
Vladyslav Hnatchenko <Vladyslav.Hnatchenko@eleks.com>
2018-12-26 16:01:13 +0200

    12/26/18

--- demo_mongodb_test.py
+++ demo_mongodb_test.py
@@ -3,12 +3,24 @@ import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydict = {
+    "name": "John",
+    "address": "Highway 37"
+}
+x = mycol.insert_one(mydict)
+
+# Return the id Field #
 
 print(myclient.list_database_names())
+
+collist = mydb.list_collection_names()
+if "customers" in collist:
+    print("The collection exists.")
 
 dblist = myclient.list_database_names()
 if "mydatabase" in dblist:
     print("The database exists.")
 else:
     print("We can't create db! \_^:^_/")
-
